a66ad1d2f1e14dcf53be4b7840356afffc699a20
cweiqi27 <chanweiqi27@gmail.com>
2022-12-25 15:04:38 +0800

    add post

--- src/components/post/PostContent.tsx
+++
@@ -1,9 +0,0 @@
-type PostContentProps = {
-  content?: string;
-};
-
-const PostContent = (props: PostContentProps) => {
-  return <p className="text-justify text-zinc-200">{props.content}</p>;
-};
-
-export default PostContent;
--- src/components/post/PostFlair.tsx
+++
@@ -1,9 +0,0 @@
-type PostFlairProps = {
-  flair: string;
-};
-
-const PostFlair = (props: PostFlairProps) => {
-  return <></>;
-};
-
-export default PostFlair;
