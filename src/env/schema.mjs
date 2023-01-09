46a7dc17282f662e03bde519a50be3acec89ba1c
cweiqi27 <chanweiqi27@gmail.com>
2023-01-10 06:20:53 +0800

    add pusher env schema

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -17,12 +17,17 @@ export const serverSchema = z.object({
     // Since NextAuth.js automatically uses the VERCEL_URL if present.
     (str) => process.env.VERCEL_URL ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
+    process.env.VERCEL ? z.string() : z.string().url()
   ),
+  // OAuth
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
+  // Pusher
+  PUSHER_APP_ID: z.string(),
+  PUSHER_APP_SECRET: z.string(),
+  PUSHER_APP_CLUSTER: z.string(),
 });
 
 /**
@@ -31,7 +36,7 @@ export const serverSchema = z.object({
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_PUSHER_APP_KEY: z.string(),
 });
 
 /**
@@ -41,5 +46,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_PUSHER_APP_KEY: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
 };
