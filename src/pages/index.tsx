d4d843f4cc451ece9c92f02f479ec297e42dc8f7
cweiqi27 <chanweiqi27@gmail.com>
2023-01-11 08:13:42 +0800

    extract auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,11 @@ import { PusherProvider } from "utils/pusher";
 import Pusher from "pusher-js";
 import PostCreate from "components/post/PostCreate";
 import PostList from "components/post/PostList";
+import Auth from "components/Auth";
 
 const Home: NextPage = (props) => {
   return (
     <Layout>
-      <AuthShowcase />
       <PostCreate />
       <div className="flex flex-col items-center gap-2">
         <PostList />
@@ -21,30 +21,6 @@ const Home: NextPage = (props) => {
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
 
 const PusherShowcase: React.FC = () => {
   const pusher = new Pusher("12bf7d67fc9ed5e498fa", {
