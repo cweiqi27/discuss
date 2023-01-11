39cc74f16ac5a36b76df7edb9490a251c76eac2c
cweiqi27 <chanweiqi27@gmail.com>
2023-01-11 08:15:03 +0800

    update PostCard styles

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,7 +1,8 @@
+import { IconMessage } from "@tabler/icons";
 import Avatar from "components/Avatar/Avatar";
 import Card from "components/layout/Card";
 import Link from "next/link";
-import { RouterOutputs } from "utils/trpc";
+import type { RouterOutputs } from "utils/trpc";
 import PostContent from "./PostContent";
 import PostFlair from "./PostFlair";
 import PostLoadingSkeleton from "./PostLoadingSkeleton";
@@ -19,32 +20,63 @@ const PostCard = (props: PostCardProps) => {
       {props.isLoading ? (
         <PostLoadingSkeleton />
       ) : (
-        <Card
-          isFlexRow
-          addStyles="background-blur gap-8 mb-2 w-full border-[0.25px] border-zinc-200/40 bg-gradient-to-br from-zinc-50/5 to-zinc-50/10 shadow"
-        >
-          {/* Left */}
-          <div className="flex flex-col justify-between gap-4">
+        <Link href="/" className="w-full">
+          <div
+            className="background-blur mb-2 grid auto-cols-fr grid-flow-row grid-cols-5 grid-rows-4 place-items-baseline gap-8 rounded border-[0.25px] border-zinc-200/40 bg-gradient-to-br from-zinc-400/5 to-zinc-400/10 p-4 shadow
+        hover:border-purple-500 sm:grid-flow-col sm:grid-cols-10 sm:grid-rows-3"
+          >
+            {/* Left */}
             <Avatar
               src={props.post.user.image ?? ""}
               alt={props.post.user.name ?? ""}
+              addStyles="order-1 place-self-start sm:place-self-center"
             />
-            <PostVote />
-          </div>
-          {/* Right */}
-          <div className="flex flex-col items-start justify-between gap-2">
-            <div className="flex gap-4">
+            <div className="order-4 place-self-center sm:order-2 sm:row-span-2">
+              <PostVote />
+            </div>
+            {/* Right */}
+            <div className="order-2 col-span-4 place-self-start sm:order-3 sm:col-span-9">
               <PostFlair flair="Fun" flairHref="/" />
             </div>
-            <Link href="/">
+            <div className="order-3 col-span-5 row-span-2 sm:order-4 sm:col-span-9 sm:row-span-1">
               <div className="flex flex-col items-start justify-center gap-2">
                 <PostTitle title={props.post.title} />
                 <PostContent description={props.post.description} />
               </div>
-            </Link>
+            </div>
+            <div className="order-5 col-span-4 sm:order-4 sm:col-span-9">
+              <div className="flex w-full justify-center gap-2">
+                <Link
+                  href="/api"
+                  className="flex-1 rounded-full px-4 py-2 text-zinc-400 hover:bg-zinc-600 hover:text-zinc-100"
+                >
+                  <div className="flex gap-2">
+                    <IconMessage />
+                    Comment
+                  </div>
+                </Link>
+                <Link
+                  href="/"
+                  className="flex-1 rounded-full px-4 py-2 text-zinc-400 hover:bg-zinc-600 hover:text-zinc-100"
+                >
+                  <div className="flex gap-2">
+                    <IconMessage />
+                    Comment
+                  </div>
+                </Link>
+                <Link
+                  href="/"
+                  className="flex-1 rounded-full px-4 py-2 text-zinc-400 hover:bg-zinc-600 hover:text-zinc-100"
+                >
+                  <div className="flex gap-2">
+                    <IconMessage />
+                    Comment
+                  </div>
+                </Link>
+              </div>
+            </div>
           </div>
-          <div>bottom</div>
-        </Card>
+        </Link>
       )}
     </>
   );
