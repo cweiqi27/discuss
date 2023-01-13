6d86360565567935a72aa4b290092f690137b8cc
cweiqi27 <chanweiqi27@gmail.com>
2023-01-13 08:11:42 +0800

    update avatar directory name

---
+++ src/components/avatar/AvatarGroup.tsx
@@ -0,0 +1,9 @@
+type AvatarGroupProps = {
+  children?: React.ReactNode;
+};
+
+const AvatarGroup = (props: AvatarGroupProps) => {
+  return <div className="grid ">AvatarGroup</div>;
+};
+
+export default AvatarGroup;
