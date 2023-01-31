97ffdd46276c681f8f8788a02fe9abfd1e05d918
cweiqi27 <chanweiqi27@gmail.com>
2023-01-31 22:30:49 +0800

    refactor notification menu

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,34 @@
 import { IconBell } from "@tabler/icons";
 import Auth from "components/Auth";
-import DropdownMenu from "components/DropdownMenu";
 import LogoLink from "components/LogoLink";
+import NotificationMenu from "components/NotificationMenu";
 import Link from "next/link";
 
 const Header = () => {
+  const notifications = [
+    {
+      id: 1,
+      text: "test",
+    },
+    {
+      id: 1,
+      text: "abcd",
+    },
+    {
+      id: 1,
+      text: "abcd",
+    },
+    {
+      id: 1,
+      text: "abcd",
+    },
+  ];
+
   return (
-    <nav className="z-50 flex w-full items-end justify-between md:px-4">
+    <nav className="z-50 flex w-full justify-between py-1 md:px-4">
       <div className="flex gap-2">
         <LogoLink />
-        <Link href="/" className="self-center">
+        {/* <Link href="/" className="self-center">
           <h1
             className="bg-clip-text align-bottom text-3xl font-extrabold
             tracking-wide text-zinc-200 hover:bg-gradient-to-br hover:from-teal-500 hover:via-purple-500
@@ -17,20 +36,10 @@ const Header = () => {
           >
             Discuss
           </h1>
-        </Link>
+        </Link> */}
       </div>
-      <div className="flex items-center">
-        <DropdownMenu
-          menuButton={
-            <IconBell
-              stroke={1}
-              className="fill-zinc-600 text-zinc-200 hover:fill-pink-500"
-            />
-          }
-        >
-          <div>idk</div>
-          <div>something</div>
-        </DropdownMenu>
+      <div className="flex items-center gap-2">
+        <NotificationMenu notifcations={notifications} />
         <Auth />
       </div>
     </nav>
