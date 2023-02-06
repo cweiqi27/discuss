0656a069d43331dd9467dea7c53c5d49b8d82a28
cweiqi27 <chanweiqi27@gmail.com>
2023-02-06 23:45:59 +0800

    refactor: change signin redirect portal

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -5,15 +5,15 @@ const Auth = () => {
   const { data: sessionData } = useSession();
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white"></p>
+    <>
       <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => signOut() : () => signIn()}
+        className="hidden rounded-full bg-white/10 px-5 py-2 font-semibold text-white no-underline transition hover:bg-white/20 sm:block"
+        onClick={sessionData ? () => signOut() : () => signIn("google")}
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>
-    </div>
+      <button className="">Sign in to comment.</button>
+    </>
   );
 };
 
