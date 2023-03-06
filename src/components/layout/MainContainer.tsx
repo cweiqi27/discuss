a97a6380622c77bd3dc4a79c44b7f4b73bc32a90
cweiqi27 <chanweiqi27@gmail.com>
2023-03-06 18:00:20 +0800

    style: change container styles

--- src/components/layout/MainContainer.tsx
+++ src/components/layout/MainContainer.tsx
@@ -4,7 +4,7 @@ type MainContainerProps = {
 
 const MainContainer = ({ children }: MainContainerProps) => {
   return (
-    <div className="grid min-h-screen grid-flow-row grid-rows-[auto_1fr_auto] gap-2 overflow-clip bg-zinc-900">
+    <div className="grid min-h-screen grid-flow-row grid-rows-[auto_1fr_auto] overflow-clip bg-zinc-900">
       {children}
     </div>
   );
