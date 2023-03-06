fcbe6bb5c571a8d64251c889dc5d72cacb4d48e3
cweiqi27 <chanweiqi27@gmail.com>
2023-03-07 00:02:26 +0800

    feat: add admin and mod monitor page

---
+++ src/pages/monitor.tsx
@@ -0,0 +1,12 @@
+import Layout from "components/layout/Layout";
+
+const MonitorPage = (props) => {
+  return (
+    <Layout type="DOUBLE">
+      <section>monitor</section>
+      <section>another monitor</section>
+    </Layout>
+  );
+};
+
+export default MonitorPage;
