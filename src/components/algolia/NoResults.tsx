f56428f6d835e9643eca0083dd514dfcf8ac22b0
cweiqi27 <chanweiqi27@gmail.com>
2023-03-15 23:59:55 +0800

    style: adjust algolia text style

--- src/components/algolia/NoResults.tsx
+++ src/components/algolia/NoResults.tsx
@@ -6,8 +6,8 @@ import {
 const NoResults = () => {
   const { indexUiState } = useInstantSearch();
   return (
-    <div className="flex flex-col gap-2">
-      <p className="text-center text-lg text-zinc-300">
+    <div className="flex flex-col gap-2 p-8">
+      <p className="text-center text-lg text-zinc-400">
         No results for{" "}
         <q className="font-semibold italic">{indexUiState.query}</q>.
       </p>
